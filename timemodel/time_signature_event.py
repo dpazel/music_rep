4aaa13b6e9267cb420c35d5b46f78c046f2bf116
dpazel <dpazel@optonline.net>
2018-04-12 10:21:18 -0400

    Ensure time is Position type in constructor.

--- timemodel/time_signature_event.py
+++ timemodel/time_signature_event.py
@@ -6,6 +6,7 @@ Purpose: Defines a time signature as an Event.
 
 """
 from timemodel.event import Event
+from timemodel.position import Position
 
 
 class TimeSignatureEvent(Event):
@@ -19,6 +20,8 @@ class TimeSignatureEvent(Event):
         
         Args:
           time_signature: (TimeSignature) object.
-          time: Comparable object.
+          time: Position.
         """
+        if not isinstance(time, Position):
+           raise Exception('time argument to TimeSignatureEvent must be Position not \'{0}\'.'.format(type(time)))
         Event.__init__(self, time_signature, time)
