f4d91755e56c718aeebf7225293bff2fb44fe448
dpazel <dpazel@optonline.net>
2021-10-01 18:41:05 -0400

    add mozart example using Italian chord.

--- transformation/harmonictranscription/t_harmonic_transcription_examples.py
+++ transformation/harmonictranscription/t_harmonic_transcription_examples.py
@@ -9,6 +9,7 @@ from tonalmodel.interval import Interval, IntervalType
 
 
 def build_hct(hc_expressed_list):
+
     parse_str = '{'
     for t in hc_expressed_list:
         parse_str += '<' + t[0] + '> qC:4 '
@@ -17,6 +18,7 @@ def build_hct(hc_expressed_list):
     lge = LineGrammarExecutor()
 
     _, hct = lge.parse(parse_str)
+
     new_hct = HarmonicContextTrack()
     for hc, t in zip(hct.hc_list(), hc_expressed_list):
         new_hc = HarmonicContext(hc.tonality, hc.chord, t[1])
@@ -150,7 +152,7 @@ def schubert_piece():
                             ('C-Major:IV', Duration(1, 8)),
                             ('C-Major:I', Duration(3, 8)),
                             ('C-Major:V', Duration(3, 8)),
-                            ('C-Major:I', Duration(1, 4)),
+                            ('C-Major:VI', Duration(1, 4)),
                             ]
     target_hct = build_hct(target_harmonic_list)
 
@@ -166,7 +168,122 @@ def schubert_piece():
 
     print_filtered_results(scored_filtered_results)
 
+def mozart_c_minor_example_with_italian():
+    from tonalmodel.tonality import Tonality
+    from tonalmodel.modality import Modality, ModalityType
+    from tonalmodel.diatonic_tone import DiatonicTone
+    from harmonicmodel.tertian_chord_template import TertianChordTemplate
+    print('----- test mozart c-minor fantasy with Italian -----')
+
+    diatonic_tonality = Tonality.create(ModalityType.Major, DiatonicTone("C"))
+    template = TertianChordTemplate.parse("CIt")
+    chord = template.create_chord(diatonic_tonality)
+    print(chord)
+
+
+    source_expression = '{<C-Natural: i> q@C:4 iEb <C-Natural: C-It> F# G Ab C <C-Melodic: V> iB:3}'
+
+    t_sub = THarmonicTranscription.create(source_expression)
+
+    target_harmonic_list = [('A-Melodic:iv', Duration(3, 4)),
+                            ('A-Natural:i', Duration(1, 4)),
+                            ('A-Melodic:V', Duration(1, 8))]
+    target_hct = build_hct(target_harmonic_list)
+
+    tag_map = {0: DiatonicPitch.parse('D:4')}
+    results = t_sub.apply(target_hct, 'B:3', tag_map, t_sub.height + 5, 100)
+
+    results_filter = MinContourFilter(t_sub.source_line, results.pitch_results)
+    scored_filtered_results = results_filter.scored_results
+
+    print('Original line:')
+    print('      ' + generate_comparative_line(t_sub.source_line))
+
+    print_filtered_results(scored_filtered_results)
+
+def mozart_GMajor_KV283():
+    print('----- test mozart KV283 -----')
+
+    #  D:5 < E:5 < G:5 < A:5 < C:6 < D:6 < E:6 < F#:6 > D:6 > C:6 > Bb:5 > A:5 > G:5 > F:5 >
+    #  E:5 > D:5 > C#:5 < E:5 > C#:5 > A:4 < B:4 < D:5 > B:4 > G:4 < A:4  score(14)
+
+    source_expression = '{<G-Major: I> sD:5 E F# G A B C:6 D <:IV> sC:6 B:5 A G F# E D C <:I> sB:4 D:5 B:4 G <:VDom7> A C:5 A:4 F#  <:I> qG}'
+    t_sub = THarmonicTranscription.create(source_expression)
+    target_harmonic_list = [('G-Melodic:v', Duration(1, 2)),
+                            ('D-Natural:i', Duration(1, 2)),
+                            ('D-Major:V', Duration(1, 4)),
+                            ('D-Major:IV', Duration(1, 4)),
+                            ('A-Melodic:i', Duration(1, 4))
+                            ]
+    target_hct = build_hct(target_harmonic_list)
+
+    tag_map = {0: DiatonicPitch.parse('D:5')}
+    results = t_sub.apply(target_hct, 'F#:4', tag_map, t_sub.height + 4, 20, tunnel_half_interval=Interval(3, IntervalType.Major))
+
+    results_filter = MinContourFilter(t_sub.source_line, results.pitch_results)
+    scored_filtered_results = results_filter.scored_results
+
+    print('Original line:')
+    print('      ' + generate_comparative_line(t_sub.source_line))
+
+    print_filtered_results(scored_filtered_results)
+
+def mozart_GMajor_KV283_with_motif():
+    print('----- test mozart KV283 with motif-----')
+    '''
+    The idea is to make the first 8 notes scalar. This cannot be done since the 6th note is flagged to be chordal.
+    However no chordal pitch on the 6th note lies on a scalar path from the initial D:5 assignment - using G-melodic
+    as the tonality.
+    '''
+    from melody.constraints.step_sequence_constraint import StepSequenceConstraint
+    from melody.structure.motif import Motif
+    from melody.structure.melodic_form import MelodicForm
+
+    #  D:5 < E:5 < G:5 < A:5 < C:6 < D:6 < E:6 < F#:6 > D:6 > C:6 > Bb:5 > A:5 > G:5 > F:5 >
+    #  E:5 > D:5 > C#:5 < E:5 > C#:5 > A:4 < B:4 < D:5 > B:4 > G:4 < A:4  score(14)
+
+    #source_expression = '{<G-Major: I> sD:5 E F# G A B C:6 D <:IV> sC:6 B:5 A G F# E D C <:I> sB:4 D:5 B:4 G <:VDom7> A C:5 A:4 F#  <:I> qG}'
+    source_expression = '{<G-Major: I> sD:5 E F# G A B C:6 D }'
+    lge = LineGrammarExecutor()
+
+    source_line, source_hct = lge.parse(source_expression)
+
+    actors = source_line.get_all_notes()
+
+    c = [
+        StepSequenceConstraint(actors[0:8], [1, 1, 1, 1, 1, 1, 1])
+    ]
+
+    m = Motif(actors, c, 'A')
+    melodic_form = MelodicForm([m])
+
+    t_sub = THarmonicTranscription(source_line, source_hct, melodic_form)
+
+    target_harmonic_list = [('G-Melodic:v', Duration(1, 2)),
+                           # ('D-Natural:i', Duration(1, 2)),
+                           # ('D-Major:V', Duration(1, 4)),
+                          #  ('D-Major:IV', Duration(1, 4)),
+                          #  ('A-Melodic:i', Duration(1, 4))
+                            ]
+    target_hct = build_hct(target_harmonic_list)
+
+    tag_map = {0: DiatonicPitch.parse('D:5')}
+    results = t_sub.apply(target_hct, 'F#:4', tag_map, t_sub.height + 20, 20, tunnel_half_interval=Interval(3, IntervalType.Major))
+
+    results_filter = MinContourFilter(t_sub.source_line, results.pitch_results)
+    scored_filtered_results = results_filter.scored_results
+
+    print('Original line:')
+    print('      ' + generate_comparative_line(t_sub.source_line))
+
+    print_filtered_results(scored_filtered_results)
+
+
 #mozart_c_minor_example()
 #schubert_a_major_v1()
 #schubert_a_major_v2()
-schubert_piece()
+#schubert_piece()
+
+#mozart_c_minor_example_with_italian()
+#mozart_GMajor_KV283()
+mozart_GMajor_KV283_with_motif()
