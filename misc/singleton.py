ef8851a43840f51a55c08f2184a0da1053d0326e
dpazel <dpazel@optonline.net>
2022-01-27 10:17:56 -0500

    delete reference comment

--- misc/singleton.py
+++ misc/singleton.py
@@ -4,7 +4,6 @@ class Singleton(object):
     This is a class that implements singleton for its subclasses.
     The technique is based on a variant of other techniques found in:
     http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
-    https://gist.github.com/werediver/4396488
 
     The technique is simply to build a map of classes to their unique instances.
     The first time called for some particular
