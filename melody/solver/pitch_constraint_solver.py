2d5b7e2b1662e96ad280c1aeee9dcdc0ab06c3b3
dpazel <dpazel@optonline.net>
2022-03-21 18:17:48 -0400

    Remove old unused code and some commented out code (from the change to OrderedSet() from set().

--- melody/solver/pitch_constraint_solver.py
+++ melody/solver/pitch_constraint_solver.py
@@ -22,11 +22,11 @@ class PitchConstraintSolver(object):
         
         :param policies: non-null set of policies
         """
-        if policies is None or (not isinstance(policies, set) and not isinstance(policies, list) and \
+        if policies is None or (not isinstance(policies, set) and not isinstance(policies, list) and
                                 not isinstance(policies, OrderedSet)):
             raise Exception('Policies must be non-null and a Set')
 
-        self._policies = OrderedSet(policies)    #set(policies)
+        self._policies = OrderedSet(policies)
 
         self.v_policy_map = dict()
         self._build_v_policy_map()
@@ -167,8 +167,8 @@ class PitchConstraintSolver(object):
                                 if len(cand_results) != 0:
                                     value_results = value_results.union(cand_results)
                                     found = True
-                                value_results.remove(r)  # r has no c_note assigned, what was returned did!
-                                                         # If not, r's peers cannot be assigned!
+                                value_results.remove(r)     # r has no c_note assigned, what was returned did!
+                                                            # If not, r's peers cannot be assigned!
                         if found is False:  # Same as if part, if c_note produces no results, it cannot be assigned.
                             break           # If one peer fails, they all will!
             results = results.union(value_results)
@@ -222,15 +222,12 @@ class PitchConstraintSolver(object):
                 returned_pitches = OrderedSet()
                 for n in p_values:
                     returned_pitches.add(n.diatonic_pitch)
-                #returned_pitches = {n.diatonic_pitch for n in p_values}
             pitches = returned_pitches if pitches is None else pitches.intersection(returned_pitches)
-
 
         retset = OrderedSet()
         for p in pitches:
             retset.add(Note(p, v_note.base_duration, v_note.num_dots))
         return retset
-        #return {Note(p, v_note.base_duration, v_note.num_dots) for p in pitches}
 
     def _candidate_closure(self, p_map, v_note):
         """
@@ -275,136 +272,3 @@ class PitchConstraintSolver(object):
         for i in range(0, len(ss)):
             s = ss[i]
             print('[{0}] {1}'.format(i, s[1]))
-
-    '''
-    Suppose we have a set of non-essential constraints.
-    
-    Start by solving on the essential ones.
-    Then:
-        For each c in non-essentials.
-           p_map = PMap()
-           pick a v_note in c.actors and get its set of solutions
-           for each result in solutions:
-              p_map[v_note] = value
-              for each peer in c.actor not v_note:
-                 solve for peer's values
-                 recurse this process until all peer's are met.
-            This give a set of pmaps that solve c
-            cmap[c] = pmaps
-            
-        Sort this map by len(pmaps) least to highest.
-       
-       
-       For each pmap in essential pmaps:
-          for each c in non-essential ordered by low #pmaps
-             for each p in cmap[c]:
-                 pmap = pmap 'ored' p 
-              recurse on this process until you meet some threshold on the solution set size.
-    '''
-
-    def solve1(self, p_map_param, accept_partials=False, instance_limit=-1):
-        """
-        Solve the constraints constraint system using p_map as the start.
-        :param p_map_param:
-        :param accept_partials:
-        :param instance_limit:
-        :return:
-        """
-        p_map = p_map_param if isinstance(p_map_param, PMap) else PMap(p_map_param)
-        self._check_p_map(p_map)
-
-        self.__instance_limit = instance_limit
-        self.__num_instances = 0  # reset
-
-        results = [p_map]
-        # list of tuples (v_note, {solution to v_note's policies})
-        starting_nodes_prime = self._build_potential_values(p_map, p_map.keys())
-        # PitchConstraintSolver.print_starting_nodes(starting_nodes_prime)
-        if len(starting_nodes_prime) == 0:
-            raise Exception('Insufficient initial information for solution')
-        starting_nodes = list()      # list of tuples (v_note, {solution to v_note's policies})
-
-        has_failures = False
-        while {a[0] for a in starting_nodes_prime} != {a[0] for a in starting_nodes} and len(starting_nodes_prime) != 0:
-            starting_nodes = starting_nodes_prime
-            results_prime = list()
-            for p_map in results:
-                results_prime.extend(self._visit(p_map, starting_nodes[0][0]))
-            results = results_prime
-            if len(results) == 0:
-                has_failures = True
-                break
-            # TODO: This is an issue when has_failures is True and so results is empty.
-            #       Idea: remove starting_nodes[0][0] and use next, and if values set is empty, drop out?
-            starting_nodes_prime = self._build_potential_values(results[0], results[0].keys())
-
-        if has_failures:
-            return results if accept_partials else list()
-
-        return results
-
-    def _visit_old(self, p_map, v_note):
-        """
-        Recursive method used to derive sets of solution to the constraints constraint problem.
-
-        :param p_map: PMap
-        :param v_note: ContextualNote, source key of PMap
-        :return: A set of pmaps
-        """
-        # print 'Entering Visit vnote={0}'.format(v_note.note.diatonic_pitch)
-        # print '   pmap = {0}'.format(p_map)
-
-        if p_map[v_note].note is not None:  # setting means visited
-            return {}
-
-        results = set()
-
-        result_values = self._build_potential_values(p_map, {v_note})
-        if len(result_values) == 0:
-            p_map[v_note].note = None
-            return results
-
-        for value in result_values[0][1]:
-            value_results = set()  # results for this value for v_note + solutions for all peers to v_mote
-            p_map[v_note].note = value
-
-            # Advantage in following peers as above setting partially solves each policy v_note is involved in.
-            peer_candidates = self._candidate_closure(p_map, v_note)
-            if len(peer_candidates) == 0:
-                pmap_full = PitchConstraintSolver.pmap_full(p_map)
-                if pmap_full and self.instance_limit != -1 and self.__num_instances >= self.instance_limit:
-                    results = results.union(value_results)
-                    return results
-
-                # We reached the end of a branch, save the result.
-                value_results.add(p_map.replicate())  # We only need a shallow copy
-                if pmap_full:
-                    self.__num_instances = self.__num_instances + 1
-
-            else:
-                for c_note in peer_candidates:
-                    if len(value_results) == 0:  # first time through, visit with p_map!
-                        value_results = self._visit(p_map, c_note)
-                        if len(value_results) == 0:  # Indicates failure to assign c_note, move to next value.
-                            break                    # If one peer fails, they all will!
-                    else:
-                        if self.instance_limit != -1 and self.__num_instances >= self.instance_limit:
-                            results = results.union(value_results)
-                            return results
-                        value_results_copy = value_results.copy()
-                        # for peer c_note, if all r below fails (len(cand_results) == 0) should we also move on to
-                        # next value? Add 'found' flag, set after union, after loop, check if False, to break
-                        found = False
-                        for r in value_results_copy:
-                            if r[c_note].note is None:
-                                cand_results = self._visit(r, c_note)
-                                if len(cand_results) != 0:
-                                    value_results = value_results.union(cand_results)
-                                    found = True
-                                value_results.remove(r)
-                        if found is False:  # Same as if part, if c_note produces not results, it cannot be assigned.
-                            break           # If one peer fails, they all will!
-            results = results.union(value_results)
-
-        p_map[v_note].note = None
-        return results
